ae741382dfb274f4ae990d8aa848c98b58f343cc
praveen-svk1408 <praveensvk1992@gmail.com>
2026-04-02 11:17:09 +0530

    updated readme

--- frontend-backup/src/components/LoadingSpinner.tsx
+++
@@ -1,7 +0,0 @@
-export default function LoadingSpinner({ className = "" }: { className?: string }) {
-  return (
-    <div className={`flex items-center justify-center ${className}`}>
-      <div className="h-8 w-8 animate-spin rounded-full border-4 border-gray-200 border-t-blue-600" />
-    </div>
-  );
-}
